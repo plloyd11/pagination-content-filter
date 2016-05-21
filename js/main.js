2cf1fb16c1295883f63b551f829688d98b5f4fee
Peter Lloyd <plloyd11@gmail.com>
2016-05-21 13:16:44 -0400

    Progress on search feature

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@
     // Create Pagination Nav Items & Search Box so they are loaded initially
     (function() {
         var studentsPerPage = 10;
-        var pagesNeeded = Math.ceil($('.student-item').length / studentsPerPage);
+        var pagesNeeded = Math.ceil($(students).length / studentsPerPage);
         $('.page').append(pagination);
         $('.page-header').append(searchBox);
         for (var i = 0; i < pagesNeeded; i++) {
@@ -15,7 +15,7 @@
             $('.pagination ul').append(navItems);
         }
         // Hide all students in the array except for the initial 10
-        $(students).hide().slice(0,10).show();
+        $(students).hide().slice(0, 10).show();
         // Add active class to first link in pagination
         $('.pagination').find('a').first().addClass('active');
     }());
@@ -36,7 +36,9 @@
     // Bind 'active' class to click event handler
     pagination.on('click', 'a', function(e) {
         e.preventDefault();
+        // store reference of user click
         var clicked = e.target;
+        // store number that corresponds to pagination nav item
         var filter = $(clicked).html();
         var studentsPerPage = 10;
         var startSlice = (filter * studentsPerPage) - studentsPerPage;
@@ -51,40 +53,43 @@
     // Search IIFE
 
     (function() {
-      var names = $('.student-item');
-      var search = $('.student-search').find('input');
-      var tempStudent = [];
+        var names = $('.student-item');
+        var search = $('.student-search').find('input');
+        var tempStudent = [];
 
-      // Loop through each students details & store container element, email address & name
-      names.each(function() {
-         tempStudent.push({
-            element: this,
-            email: $(this).find('span').html().trim(),
-            text: $(this).find('h3').html().trim()
-         });
-      });
+        // Loop through each students details & store container element, email address & name
+        names.each(function() {
+            tempStudent.push({
+                element: this,
+                email: $(this).find('span').html().trim(),
+                text: $(this).find('h3').html().trim()
+            });
+        });
 
-      console.log(tempStudent);
+        // Filter through all the items in the tempStudent Array
+        function filter() {
+            // Store the search results
+            var query = this.value.trim().toLowerCase();
 
-      function filter() {
-         var query = this.value.trim().toLowerCase();    // Store the search results
-
-         tempStudent.forEach(function(name) {
-            var index = 0;
-            if (query) {
-               index = name.text.indexOf(query);
-               // Gotta add email to the if statement
+            // For all the student items
+            tempStudent.forEach(function(name) {
+                var index = 0;
+                if (query) {
+                    index = name.text.indexOf(query);
+                }
+                name.element.style.display = index === -1 ? 'none' : '';
+            });
+            if (query.length === 0) {
+                $(students).hide().slice(0, 10).show();
             }
-            name.element.style.display = index === -1 ? 'none' : '';
-         });
-      }
+        }
 
-      if ('oninput' in search[0]) {
-         search.on('input', filter);
-      } else {
-         search.on('keyup', filter);
-      }
+        if ('oninput' in search[0]) {
+            search.on('input', filter);
+        } else {
+            search.on('keyup', filter);
+        }
 
-   }());
+    }());
 
 })(jQuery);
